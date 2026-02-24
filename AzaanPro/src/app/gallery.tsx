9cffdfb723f985192b7dc71940b149b3e888b322
Sk_Alamin <alamin_sk@trisysit.com>
2026-02-24 15:47:35 +0530

    lot's func added with initial ui with bugs solved

---
+++ AzaanPro/src/app/gallery.tsx
@@ -0,0 +1,8 @@
+import { View, Text} from "react-native";
+export default function GalleryScreen() {
+  return (
+    <View>
+      <Text>Gallery</Text>
+    </View>
+  );
+}
---
+++ AzaanPro/src/app/names.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function NamesScreen() {
+  return (
+    <View>
+      <Text>Names of Allah</Text>
+    </View>
+  );
+}
